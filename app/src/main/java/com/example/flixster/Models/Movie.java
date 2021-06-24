785540baa7894f6e1c42d44b5151f388d161e4e9
Ricardo Glez Leal <53849239+RicardoGLeal@users.noreply.github.com>
2021-06-23 22:15:12 -0500

    Expose the details of a movie
    -Now the user can clicks in one of the movies in the list and a new page will open with the movie selected and the details of the movie (rating and synopsis). Implement this by using an Intent to go to a separate activity, displaying the poster picture, synopsis, and the rating using a RatingBar.

--- app/src/main/java/com/example/flixster/Models/Movie.java
+++ app/src/main/java/com/example/flixster/Models/Movie.java
@@ -3,22 +3,29 @@ package com.example.flixster.Models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 //POJO for the movies.
+@Parcel //class Parcelable
 public class Movie {
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    Double voteAverage;
+
+    // no-arg, empty constructor required for Parceler
+    public Movie() {}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         backdropPath = jsonObject.getString("backdrop_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        voteAverage = jsonObject.getDouble("vote_average");
     }
 
     //iterates through the json array and constructs a movie for each element in the JSON Array
@@ -44,5 +51,9 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
     }
 }
--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,9 +15,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.flixster.Models.Movie;
+import com.example.flixster.MovieDetailsActivity;
 import com.example.flixster.R;
 
 import org.jetbrains.annotations.NotNull;
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -55,17 +58,36 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         return movies.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         TextView title_txt;
         TextView overview_txt;
         ImageView ivPoster;
 
-        public ViewHolder(@NonNull @NotNull View itemView) {
+        public ViewHolder(View itemView) {
             super(itemView);
             title_txt = itemView.findViewById(R.id.title_txt);
             overview_txt = itemView.findViewById(R.id.overview_txt);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            // add this as the itemView's OnClickListener
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            // gets item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the movie at the position, this won't work if the class is static
+                Movie movie = movies.get(position);
+                // create intent for the new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                // serialize the movie using parceler, use its short name as a key
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                // show the activity
+                context.startActivity(intent);
+            }
         }
 
         public void bind(Movie movie) {
